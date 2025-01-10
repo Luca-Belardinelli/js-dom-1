5847d362baa698e54fb3cff8db286a40240b62da
Luca Belardinelli <belardinelliluca@gmail.com>
2025-01-10 17:24:09 +0100

    [test_js]

---
+++ script.js
@@ -0,0 +1,16 @@
+
+// selezione elementi 
+
+const button = document.getElementById('interrutore');
+
+const img =  document.getElementById('spenta')
+
+
+// gestione del click
+
+button.addEventListener('click', function() {
+
+    spenta.src = 'yellow_lamp.png';
+    interrutore.textContent = 'Spegni';
+
+});
