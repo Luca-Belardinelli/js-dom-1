b0f675077ebdb834d8ad68f5e24bb118f84d0e18
Luca Belardinelli <belardinelliluca@gmail.com>
2025-01-13 09:48:17 +0100

    [bonus]

--- script.js
+++ script.js
@@ -6,15 +6,30 @@ const button = document.getElementById('interrutore');
 const spenta =  document.getElementById('spenta')
 const accesa =  document.getElementById('accesa')
 
+let isOn = false;
 
 // gestione del click
 
 button.addEventListener('click', function() {
+
+    if(!isOn){
 
     spenta.classList.add('accesa');
 
     accesa.classList.remove('accesa');
 
     interrutore.textContent = 'Spegni';
+
+    }else{
+
+    spenta.classList.remove('accesa');
+
+    accesa.classList.add('accesa');
+
+    interrutore.textContent = 'Accendi';
+}
+
+// Inverto stato lampadina
+isOn = !isOn;
 
 });
